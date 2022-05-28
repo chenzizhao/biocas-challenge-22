b7361bcb6da6f88f4cf44ebc1ea079c2fe18496f
chenzizhao <zoe.chenzizhao@gmail.com>
2022-05-28 15:42:10 -0400

    remove YesNo

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -8,35 +8,6 @@ from data.SPRSound.preprocess import preprocess
 from torchaudio import datasets, load
 import importlib
 
-class YesNoDataLoader(BaseDataLoader):
-    """
-    Our audio-equivalent of MNIST
-    """
-
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-
-        def collate_fn(batch):
-            tensors, targets = [], []
-
-            # Gather in lists, and encode labels as indices
-            for waveform, sample_rate, label in batch:
-                tensors += [waveform]
-                targets += [torch.Tensor(label)]
-
-            # Group the list of tensors into a batched tensor
-            #   hack to ensure always 55840
-            tensors[0] = F.pad(tensors[0], (0, 55840-tensors[0].shape[-1]), mode='constant', value=0.)
-            tensors = _pad_sequence(tensors)
-            tensors.squeeze_(1) # 54x55840
-            
-            targets = torch.stack(targets) # 54x8
-
-            return tensors, targets
-
-        self.data_dir = data_dir
-        self.dataset = datasets.YESNO(self.data_dir, download=True)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, collate_fn=collate_fn)
-
 
 # Important Assumption (used in model/metric.py)
 # Normal is always index 0
--- model/model.py
+++ model/model.py
@@ -2,18 +2,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 
-
-class YesNoModel(BaseModel):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(55840, 128)
-        self.fc2 = nn.Linear(128, 8)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.tanh(self.fc2(x))
-        return x
-
 class Resp21Model(BaseModel):
     def __init__(self):
         super().__init__()
