2646c44510560eecd037caf29c7724efbc753c65
BaronWang0130 <88529547+BaronWang0130@users.noreply.github.com>
2022-06-02 22:03:49 +0800

    Update model.py
    update bi-resnet model

--- model/model.py
+++ model/model.py
@@ -61,3 +61,89 @@ class LightCNN(BaseModel):
         )
 
         return self.dens_model(self.cnn_model(x))
+    
+    
+    
+
+    class BilinearCNN(nn.Module):
+    
+    def __init__(self,dim):
+        super(BilinearCNN, self).__init__()
+        
+        self.conv0 = nn.Conv2d(1, 64, 3, 1)
+        self.conv1 = nn.Conv2d(1, 64, 3, 1)
+        self.ResNet_0_0 = ResBlock(64, 64, stride=2, downsample=conv1x1(64, 64, 2))
+        self.ResNet_0_1 = ResBlock(64, 64, stride=2, downsample=conv1x1(64, 64, 2))
+        self.ResNet_1_0 = ResBlock(64, 64, stride=2, downsample=conv1x1(64, 64, 2))
+        self.ResNet_1_1 = ResBlock(64, 64, stride=2, downsample=conv1x1(64, 64, 2))
+        self.ResNet_0 = ResBlock(64, 64)
+        self.ResNet_1 = ResBlock(64, 64)
+        self.ResNet_2 = ResBlock(64, 64)
+        self.ResNet_3 = ResBlock(64, 64)
+        self.ResNet_4 = ResBlock(64, 64)
+        self.ResNet_5 = ResBlock(64, 64)
+        self.ResNet_6 = ResBlock(64, 64)
+        self.ResNet_7 = ResBlock(64, 64)
+        self.ResNet_8 = ResBlock(64, 64)
+        self.ResNet_9 = ResBlock(64, 64)
+        self.ResNet_10 = ResBlock(64, 64)
+        self.ResNet_11 = ResBlock(64, 64)
+        self.ResNet_12 = ResBlock(64, 64)
+        self.ResNet_13 = ResBlock(64, 64)
+        self.ResNet_14 = ResBlock(64, 64)
+        self.ResNet_15 = ResBlock(64, 64)
+        self.ResNet_16 = ResBlock(64, 64)
+        self.ResNet_17 = ResBlock(64, 64)
+        self.norm0 = norm(dim)
+        self.norm1 = norm(dim)
+        self.relu0 = nn.ReLU(inplace=True)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool0 = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool1 = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = nn.Linear(64, 4)
+        self.dropout = nn.Dropout(args.dropout)
+        self.flat = Flatten()
+        
+        
+    def forward(self,stft,mfcc):
+        
+        out_s = self.conv0(stft)
+        out_s = self.ResNet_0_0(out_s)
+        out_s = self.ResNet_0_1(out_s)
+        out_s = self.ResNet_0(out_s)
+        out_s = self.ResNet_2(out_s)
+        out_s = self.ResNet_4(out_s)
+        out_s = self.ResNet_6(out_s)
+        out_s = self.ResNet_8(out_s)
+        out_s = self.ResNet_10(out_s)
+        out_s = self.ResNet_12(out_s)
+#        out_s = self.ResNet_14(out_s)
+#        out_s = self.ResNet_16(out_s)
+        out_s = self.norm0(out_s)
+        out_s = self.relu0(out_s)
+        out_s = self.pool0(out_s)
+        
+        out_m = self.conv1(mfcc)
+        out_m = self.ResNet_1_0(out_m)
+        out_m = self.ResNet_1_1(out_m)
+        out_m = self.ResNet_1(out_m)
+        out_m = self.ResNet_3(out_m)
+        out_m = self.ResNet_5(out_m)
+        out_m = self.ResNet_7(out_m)
+        out_m = self.ResNet_9(out_m)
+        out_m = self.ResNet_11(out_m)
+        out_m = self.ResNet_13(out_m)
+#        out_m = self.ResNet_15(out_m)
+#        out_m = self.ResNet_17(out_m)
+        out_m = self.norm1(out_m)
+        out_m = self.relu1(out_m)
+        out_m = self.pool1(out_m)
+
+        out = torch.matmul(out_s,out_m)
+
+#        out = torch.bmm(out_s, torch.transpose(out_m, 1, 2))
+        out = self.flat(out)
+        out = self.linear(out)
+        out = self.dropout(out)
+       
+        return out
