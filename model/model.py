3d4ddb7773f59e57c18b9b25b3939d1941d754ac
chenzizhao <zoe.chenzizhao@gmail.com>
2022-06-05 00:14:20 -0400

    Include preprocess step; remove padding hacks and other data loaders for now

--- model/model.py
+++ model/model.py
@@ -5,8 +5,7 @@ from base import BaseModel
 class Resp21Model(BaseModel):
     def __init__(self):
         super().__init__()
-        self.fc = nn.Linear(122880, 3)
-        # TODO
+        self.fc = nn.Linear(200, 3)
 
     def forward(self, x):
         return self.fc(x)
