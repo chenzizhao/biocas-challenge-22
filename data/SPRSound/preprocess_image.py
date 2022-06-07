8f5e56bdcd1008f21736105ee73e4603268cf552
BaronWang0130 <88529547+BaronWang0130@users.noreply.github.com>
2022-06-07 10:49:13 +0800

    Update preprocess_image.py

--- data/SPRSound/preprocess_image.py
+++ data/SPRSound/preprocess_image.py
@@ -1,13 +1,14 @@
 '''
-resize image to 224*224
+resize image to 224*224, return the tensor
 '''
-
-import cv2
-
-def preprocess(image):
+import torchvision.transforms as transforms
+import cv2 as cv
+        
+def preprocess_img(image):
 
     img=cv2.imread(image)
     process=cv2.resize(img,(224,224))
+    transf = transforms.ToTensor()
+    img_tensor = transf(img)
     
-    return process
-
+    return img_tensor
