7bc78a7436b76f3028016ec6070a355300b31862
chenzizhao <zoe.chenzizhao@gmail.com>
2022-06-08 01:38:15 -0400

    drop .item() to admit nan caused by division by zero

--- model/metric.py
+++ model/metric.py
@@ -5,7 +5,7 @@ def accuracy(output, target):
     with torch.no_grad():
         pred = torch.argmax(output, dim=1)
         assert pred.shape[0] == len(target)
-        correct = torch.sum(pred == target).item()
+        correct = torch.sum(pred == target)
     return correct / len(target)
 
 def sensitivity(output, target, task=None):
@@ -22,8 +22,8 @@ def sensitivity(output, target, task=None):
         is_adv = target >= task
         # Task 1 does not have PQ, therefore Normal is labeled as 0
         # Task 2 has PQ, labeled at 1, which we also want to drop
-        correct = torch.sum((pred == target)[is_adv]).item()
-    return correct / torch.sum(is_adv).item()
+        correct = torch.sum((pred == target)[is_adv])
+    return correct / torch.sum(is_adv)
 
 def sensitivity_task1(output, target):
     return sensitivity(output, target, task=1)
@@ -41,15 +41,14 @@ def speficity(output, target):
         pred = torch.argmax(output, dim=1)
         assert pred.shape[0] == len(target)
         is_normal = target == 0
-        correct = torch.sum((pred == target)[is_normal]).item()
-    return correct / torch.sum(is_normal).item()
+        correct = torch.sum((pred == target)[is_normal])
+    return correct / torch.sum(is_normal)
 
 def score(output, target, se_fn):
     se = se_fn(output, target)
     sp = speficity(output, target)
     avg_score = (se + sp) / 2
     har_score = (2*se*sp) / (se + sp)
-    # TODO: division by zero error
     return (avg_score + har_score) / 2
 
 def score_task1(output, target):
