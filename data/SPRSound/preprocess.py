b903b96d59533cf64ec262c5ab86e1de1867df40
chenzizhao <zoe.chenzizhao@gmail.com>
2022-06-08 01:37:03 -0400

    add stft

--- data/SPRSound/preprocess.py
+++ data/SPRSound/preprocess.py
@@ -6,7 +6,7 @@ Recording (Task 2-1 and Task 2-2):
 - preprocess_norm,
 - maybe (preprocee_stft, preprocess_mel, preprocess_wavelet)
 
-Event (Task 1-1 and Task 2-1):
+Event (Task 1-1 and Task 1-2):
 - preprocee_stft
 - preprocess_mel
 - preprocess_wavelet
@@ -19,6 +19,7 @@ import numpy as np
 import torch
 from torchaudio import load
 import pywt
+import librosa
 import librosa.display as display
 from scipy.signal import butter, lfilter
 from os import listdir
@@ -66,19 +67,10 @@ def reshape(matrix):
     out = matrix.reshape((length,length))
     return out
 
-def preprocess_wavelet(wav, sample_freq=8000):
-    sig = wav
-    fs = sample_freq
-    sig = normalize(sig)
-    if fs>4000:
-        sig = butter_bandpass_filter(sig, 1, 3999, fs, order=3)
-    wave = wavelet(sig)
-    xmax = max(wave)
-    xmin = min(wave)
-    wave = (255-0)*(wave-xmin)/(xmax-xmin)+0
-    wave = reshape(wave)
-    display.specshow(wave)
-    plt.figure(figsize=(2.24, 2.24), dpi=100)
+def _display_to_tensor_routine(size=(224, 224)):
+    """
+    This is a subroutine to convert plt figures to an image tensor, with size torch.Size([3, 224, 224])
+    """
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
     plt.subplots_adjust(top=1,bottom=0,left=0,right=1,hspace=0,wspace=0)
@@ -91,21 +83,50 @@ def preprocess_wavelet(wav, sample_freq=8000):
         plt.close()
         img = plt.imread(tmp.name + '.png')
     
-    # The image is not terribly useful though. Try uncomment the following:
-    
-    plt.imshow(img)
-    plt.show()
-
     # torch.Size([H, W, C])
     img_tensor = torch.from_numpy(np.array(img))
-    
     # convert RGBA to RGB by dropping the last channel
     img_tensor = img_tensor[:,:,[0,1,2]]
-    
     # torch.Size([C, H, W])
     img_tensor = torch.permute(img_tensor, (2, 0, 1))
+    # interpolate to get the desired size
+    img_tensor.unsqueeze_(0)
+    img_tensor = torch.nn.functional.interpolate(img_tensor, size=size)
+    img_tensor.squeeze_(0)
     
+    # # To view the image itself, try uncomment the following:
+    # plt.imshow(torch.permute(img_tensor, (1, 2, 0)))
+    # plt.show()
+
+    # assert img_tensor.shape == (3, size[0], size[1])
+
     # Float between [0, 1]
+    return img_tensor
+
+
+def preprocess_stft(wav, sample_freq=8000):
+    sig = wav
+    fs = sample_freq
+    sig = normalize(sig)
+    sig = butter_bandpass_filter(sig, 1, 3999, fs, order=3)
+    wav = librosa.stft(sig, n_fft=int(0.02*fs), hop_length=int(0.01*fs), window='hann')
+    wav = librosa.amplitude_to_db(wav[0:int(len(wav)/2),:],ref=np.max)
+    display.specshow(wav, y_axis='log',x_axis='time')
+    img_tensor = _display_to_tensor_routine()
+    return img_tensor
+
+def preprocess_wavelet(wav, sample_freq=8000):
+    sig = wav
+    fs = sample_freq
+    sig = normalize(sig)
+    sig = butter_bandpass_filter(sig, 1, 3999, fs, order=3)
+    wave = wavelet(sig)
+    xmax = max(wave)
+    xmin = min(wave)
+    wave = (255-0)*(wave-xmin)/(xmax-xmin)+0
+    wave = reshape(wave)
+    display.specshow(wave)
+    img_tensor = _display_to_tensor_routine()
     return img_tensor
 
 def preprocess(wav):
@@ -115,16 +136,17 @@ def preprocess(wav):
     --------------------
     This is a simple wrap function to provide a unifying API
     """
-    return preprocess_wavelet(wav, sample_freq=8000)
+    return preprocess_stft(wav, sample_freq=8000)
 
 if __name__ == '__main__':
-    WAV_DIR = "wav"
+    REC_DIR = "wav"
+    CLIP_DIR = "clip"
     PROC_DIR = "processed"
 
-    for wav_name in tqdm(listdir(WAV_DIR)):
-        wav, _ = load(join(WAV_DIR, wav_name))
-        wav = wav.squeeze().cpu().detach().numpy()
-        processed = preprocess(wav)
-        torch.save(processed, join(PROC_DIR, wav_name))
-        # print(processed.shape)
-        # break
+    for dir in (REC_DIR, CLIP_DIR):
+        print(f"Processing waves in {dir}/ folder")
+        for wav_name in tqdm(listdir(dir)):
+            wav, _ = load(join(dir, wav_name))
+            wav = wav.squeeze().cpu().detach().numpy()
+            processed = preprocess(wav)
+            torch.save(processed, join(PROC_DIR, wav_name))
