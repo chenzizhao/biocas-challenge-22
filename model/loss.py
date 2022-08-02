a214e41ad23159bf3d580c0d9a34092b5cf1c92c
chenzizhao <zoe.chenzizhao@gmail.com>
2022-08-02 13:26:23 -0400

    Introduce weighed loss functions

--- model/loss.py
+++ model/loss.py
@@ -1,10 +1,13 @@
+import torch
 import torch.nn.functional as F
-from torch import nn
-
-
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
 
 mse_loss = F.mse_loss
 
-cross_entropy = nn.CrossEntropyLoss()
+nll_loss = F.nll_loss
+
+def cross_entropy(output, target, device=None, weight=None):
+    if weight is None:
+        return F.cross_entropy(output, target)
+    else:
+        weight = torch.tensor(weight, dtype=torch.float, device=device)
+        return F.cross_entropy(output, target, weight=weight)
