bac8bf97230d05e4b1590737371a4b7c384c21d7
chenzizhao <zoe.chenzizhao@gmail.com>
2022-08-03 01:40:48 -0400

    Add wav datasets

--- data/SPRSound/Datasets.py
+++ data/SPRSound/Datasets.py
@@ -2,13 +2,21 @@ from os.path import join
 from torch.utils.data import Dataset
 import pandas as pd
 import torch
+import torchaudio
 
 class RespDataset(Dataset):
-    def __init__(self, data_dir, task=1):
+    def __init__(self, data_dir, task, raw=False):
         assert task in (1,2)
         self.task = task
         self.csv = pd.read_csv(join(data_dir, f'task{task}.csv'))
-        self.dir = join(data_dir, 'processed')
+        self.raw = raw
+        if raw is True:
+            if task == 1:
+                self.dir = join(data_dir, 'clip')
+            else:
+                self.dir = join(data_dir, 'wav')
+        else:
+            self.dir = join(data_dir, 'processed')
 
     def __len__(self):
         return len(self.csv)
@@ -17,11 +25,14 @@ class RespDataset(Dataset):
         entry = self.csv.iloc[index]
         wav_name = entry['wav_name']
         target = (entry[f'label_{self.task}1'], entry[f'label_{self.task}2'])
-        wav = torch.load(join(self.dir, wav_name))
+        if self.raw is True:
+            wav, _ = torchaudio.load(join(self.dir, wav_name))
+        else:
+            wav = torch.load(join(self.dir, wav_name))
         return wav, target
 
 if __name__ == "__main__":
-    r = RespDataset('.', task=1)
+    r = RespDataset('.', task=1, raw=True)
     print(f"There are {len(r)} samples in the dataset.")
     wav, target = r[0]
     print(target)
